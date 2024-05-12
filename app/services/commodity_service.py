daf574187cd5a5cfa90787401419db96cabbb9a8
KOSASIH <kosasihg88@gmail.com>
2024-05-12 17:46:35 +0700

    Create commodity_service.py

---
+++ app/services/commodity_service.py
@@ -0,0 +1,8 @@
+# Define the Commodity service
+class CommodityService:
+    def __init__(self):
+        self.commodities = []
+
+    def get_all_commodities(self):
+        # Return a list of commodities
+        return self.commodities
