4731046358f7ecc01e4a2053a57fae8d1baf770f
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:03:17 +0700

    Create normalization.py

---
+++ data_processing/normalization.py
@@ -0,0 +1,7 @@
+def min_max_scaler(data: pd.DataFrame, column_names: List[str]) -> pd.DataFrame:
+    """Normalize the given columns using min-max scaling"""
+    pass
+
+def z_score_scaler(data: pd.DataFrame, column_names: List[str]) -> pd.DataFrame:
+    """Normalize the given columns using z-score scaling"""
+    pass
