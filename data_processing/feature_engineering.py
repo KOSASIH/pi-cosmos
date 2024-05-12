b402523c8d9c66bdf35bc8225ef2282df6b7b659
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:14:22 +0700

    Create feature_engineering.py

---
+++ data_processing/feature_engineering.py
@@ -0,0 +1,7 @@
+def create_time_features(data: pd.DataFrame, column_names: List[str]) -> pd.DataFrame:
+    """Create time features from the given columns"""
+    pass
+
+def create_interaction_features(data: pd.DataFrame, column_names: List[str]) -> pd.DataFrame:
+    """Create interaction features from the given columns"""
+    pass
