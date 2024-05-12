1413504d44e0305b65d1c02c80581c3d96df3cf3
KOSASIH <kosasihg88@gmail.com>
2024-05-12 20:10:49 +0700

    Create inception_v3.py

---
+++ computer_vision/models/image_recognition/inception_v3.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.applications import InceptionV3
+
+# Load the pre-trained Inception v3 model
+base_model = InceptionV3(weights='imagenet', include_top=False)
+
+# Add a global average pooling layer
+x = layers.GlobalAveragePooling2D()(base_model.output)
+
+# Add a dense layer with 1024 units and ReLU activation
+x = layers.Dense(1024, activation='relu')(x)
+
+# Add a dropout layer with a rate of 0.5
+x = layers.Dropout(0.5)(x)
+
+# Add a dense layer with the number of classes as the number of units and softmax activation
+output = layers.Dense(num_classes, activation='softmax')(x)
+
+# Create the model
+model = tf.keras.Model(inputs=base_model.input, outputs=output)
+
+# Compile the model
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
