8af227d1cd2d90c0a28c3be40ca5d15b0f3283f8
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:19:30 +0700

    Create named_entity_recognition.py

---
+++ nlp/named_entity_recognition.py
@@ -0,0 +1,3 @@
+def named_entity_recognition(text: str, model: sklearn.base.BaseEstimator) -> List[Tuple[str, str]]:
+    """Recognize named entities in the given text using the given model"""
+    pass
