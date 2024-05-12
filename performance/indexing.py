56afed2961daacd9062b91af45ec9e190b4dbb60
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:24:03 +0700

    Create indexing.py

---
+++ performance/indexing.py
@@ -0,0 +1,7 @@
+def create_index(data: pd.DataFrame, column_names: List[str]) -> None:
+    """Create an index on the given columns of the given data"""
+    pass
+
+def query_index(data: pd.DataFrame, query: Dict[str, Any]) -> pd.DataFrame:
+    """Query the indexed data using the given query"""
+    pass
