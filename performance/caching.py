6b837668309edc14ddd66ff8792f97fa325e318f
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:08:53 +0700

    Create caching.py

---
+++ performance/caching.py
@@ -0,0 +1,7 @@
+def authenticate_user(username: str, password: str) -> bool:
+    """Authenticate the given user using the given username and password"""
+    pass
+
+def authorize_user(username: str, action: str) -> bool:
+    """Authorize the given user to perform the given action"""
+    pass
