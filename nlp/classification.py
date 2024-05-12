a190405f2dc6f9838b7e25e9818fdaf07e7ff964
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:08:01 +0700

    Create classification.py

---
+++ nlp/classification.py
@@ -0,0 +1,7 @@
+def text_classifier(text: str, model: sklearn.base.BaseEstimator) -> str:
+    """Classify the given text using the given model"""
+    pass
+
+def sentiment_analysis(text: str, model: sklearn.base.BaseEstimator) -> str:
+"""Analyze the sentiment of the given text using the given model"""
+    pass
