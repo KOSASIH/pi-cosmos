3d84471d79bfac6340f583ee89b9df292d59ddaa
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:21:35 +0700

    Create encryption.py

---
+++ security/encryption.py
@@ -0,0 +1,7 @@
+def encrypt_data(data: Any, key: str) -> Any:
+    """Encrypt the given data using the given key"""
+    pass
+
+def decrypt_data(data: Any, key: str) -> Any:
+    """Decrypt the given data using the given key"""
+    pass
