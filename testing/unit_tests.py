27ddb4537a86c77da2fce24d5d4d1f1df76a0d00
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:09:34 +0700

    Create unit_tests.py

---
+++ testing/unit_tests.py
@@ -0,0 +1,7 @@
+def test_min_max_scaler() -> None:
+    """Test the min-max scaler function"""
+    pass
+
+def test_cross_val_score() -> None:
+    """Test the cross-validation score function"""
+    pass
