aa1ae88f82ac72048915029e2e831f81287a79d8
KOSASIH <kosasihg88@gmail.com>
2024-05-12 17:45:00 +0700

    Create commodity.py

---
+++ app/models/commodity.py
@@ -0,0 +1,6 @@
+# Define the Commodity model
+class Commodity:
+    def __init__(self, id, name, description):
+        self.id = id
+        self.name = name
+        self.description = description
