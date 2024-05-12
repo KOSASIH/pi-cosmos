efe73003e8d3b0bd51db41813033de3705db38a1
KOSASIH <kosasihg88@gmail.com>
2024-05-12 19:15:54 +0700

    Create model_training.py

---
+++ machine_learning/model_training.py
@@ -0,0 +1,7 @@
+def train_model(model: sklearn.base.BaseEstimator, X: pd.DataFrame, y: pd.Series) -> sklearn.base.BaseEstimator:
+    """Train the given model on the given data"""
+    pass
+
+def evaluate_model(model: sklearn.base.BaseEstimator, X: pd.DataFrame, y: pd.Series) -> Dict[str, Any]:
+    """Evaluate the given model on the given data"""
+    pass
